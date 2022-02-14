ef8aa2e81e748fd9511985b86cea00aa5f6c2eb9
Griffin Wimminger <impendingdelusion@gmail.com>
2022-02-13 17:33:52 -0800

    placing images in src folder because they're not currently showing up on deployed site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {HashRouter as Router, Routes, Route, Link} from "react-router-dom"
+
+import AboutMe from "./components/AboutMe"
+import Experience from "./components/Experience"
+import Portfolio from "./components/Portfolio"
+import Extracurriculars from "./components/Extracurriculars"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <nav className="app-header">
+          <Link to={"/"} className="nav-item">About Me </Link>
+          <Link to={"experience"} className="nav-item">Experience </Link>
+          <Link to={"portfolio"} className="nav-item">Portfolio </Link>
+          <Link to={"extracurriculars"} className="nav-item">Extracurriculars</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="extracurriculars" element={<Extracurriculars />} />
+        </Routes>
+        
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Extracurriculars.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Extracurriculars() {
+
+    return (
+        <div className="extracurriculars component">
+            
+        </div>
+    )
+}
+
+export default Extracurriculars
